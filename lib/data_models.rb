828bbc433e17a3a7ea42b317671b584f9c8a8bb5
jnillo <juan.vazquez@betybyte.com>
2011-06-20 20:51:15 +0200

    DataModels library

---
+++ lib/data_models.rb
@@ -0,0 +1,28 @@
+module DataModels
+    
+  
+   def self.load_models
+  
+     models = []
+
+     Dir["#{RAILS_ROOT}/app/models/**/*.rb"].each do |file|
+       models << file.gsub(RAILS_ROOT+'/app/models',"").gsub('.rb','').classify.gsub(":","")
+     end
+
+     models
+  
+  end
+
+  def self.model_attributes(model)
+  	
+  	clase = eval model
+  	
+  	clase.columns.map{|att| [att.name,att.type]}
+  	
+  end
+
+  def self.build_models_yaml_file
+
+  	self.models
+
+end
