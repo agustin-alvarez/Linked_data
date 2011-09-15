f03354ce032a35bc1d433b3a6eba0691188e0771
jnillo <juan.vazquez@betybyte.com>
2011-09-15 09:31:51 +0200

    Changes in Linked Data graphs any layouts jobs

--- lib/tasks/testing_installation.rb
+++ lib/tasks/testing_installation.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm easy_data-0.0.4.gem
+rm easy_data-0.0.*
 gem build easy_data.gemspec
 gem uninstall easy_data
-gem install ./easy_data-0.0.4.gem
+gem install ./easy_data-0.0.5.gem
